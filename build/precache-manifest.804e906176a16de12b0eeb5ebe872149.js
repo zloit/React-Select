8db1b4d153f964473627f7a2cb936b3c37440fee
Тимур <timurrrik@gmail.com>
2020-02-19 21:52:09 +0700

    Build

---
+++ build/precache-manifest.804e906176a16de12b0eeb5ebe872149.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f82a3cdfd8123b957c0b2bac716d990",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cba6696afb1d43bec2bb",
+    "url": "./static/css/main.33c78a92.chunk.css"
+  },
+  {
+    "revision": "bbe9f7f4c15ac9caa343",
+    "url": "./static/js/2.f377e6cb.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.f377e6cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cba6696afb1d43bec2bb",
+    "url": "./static/js/main.ec28efab.chunk.js"
+  },
+  {
+    "revision": "f1819f30245fca343d9b",
+    "url": "./static/js/runtime-main.4a84bab3.js"
+  }
+]);
